463ce4dfb764f9aaa3e5354dbe22f51519f12635
snowlizard <illegal_burrito@live.com>
2022-03-28 23:50:22 -0700

    fixed camera angle on aya

--- src/3dmodels/aya.js
+++ src/3dmodels/aya.js
@@ -14,31 +14,32 @@ const height = 500;
 
 export const Aya = () => {
     scene = new THREE.Scene();
-    scene.background = new THREE.Color(0xdddddd);
+    scene.background = new THREE.Color( 0xdddddd );
 
     camera = new THREE.PerspectiveCamera(60, width/height, 1, 1000);
-    camera.position.set(0, 0, 0);
+    camera.position.set(0, 440, 250);
 
     scene.add(new THREE.AxesHelper(500));
 
-    renderer = new THREE.WebGLRenderer();
-    canvas.appendChild(renderer.domElement);
+    renderer = new THREE.WebGLRenderer({canvas});
     renderer.setSize(width, height);
 
     controls = new OrbitControls(camera, renderer.domElement);
-
+    
     spotLight = new THREE.SpotLight(0xffa95c, 0.5);
     spotLight.castShadow = true;
     scene.add(spotLight);
 
     const loader = new OBJLoader();
-
+    
     loader.load(aya, (obj) => {
-        model = obj
-        model.scale.set(2, 2, 2);
+        model = obj;
+        model.position.set(0, 0, 0);
+        model.scale.set(0.2, 0.2, 0.2);
         scene.add(model);
-    }, undefined,
-    (error) => console.log(`Error: ${error}`) );
+    },
+    undefined,
+    (error) => console.log(`Error: ${error}`));
 
     animate();
 }
